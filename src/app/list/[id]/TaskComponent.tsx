c963ebf693463d46d9ef1f73b359c4e5879f3691
Lucas Ike <lucasmarques@proton.me>
2024-01-23 20:08:16 +0100

    notification layout

--- src/app/list/[id]/TaskComponent.tsx
+++ src/app/list/[id]/TaskComponent.tsx
@@ -1,7 +1,6 @@
-import { Task } from "@/business/Task";
-import { useState } from "react";
-import { Bell, Check, Trash, X } from "react-feather";
-import { inspect } from "util";
+import { NotificationType, Task } from "@/business/Task";
+import React, { useEffect, useRef, useState } from "react";
+import { Bell, BellOff, Check, Trash, X } from "react-feather";
 import styles from "./taskcomponent.module.scss";
 
 const TaskComponent = ({
@@ -16,58 +15,150 @@ const TaskComponent = ({
   switchTaskStatus: () => void;
 }) => {
   const [title, setTitle] = useState(task.title);
-
+  const [notificationMenuActive, setNotificationMenuActive] = useState(false);
+  const [notificationMode, _setNotificationMode] = useState(task.notify);
+  const [textAreaElement, setTextAreaElement] =
+    useState<HTMLTextAreaElement | null>(null);
+  const [textAreaFocused, _setTextAreaFocused] = useState(false);
+  const setTextAreaFocused = (tv: boolean) => {
+    console.log(tv);
+    _setTextAreaFocused(tv);
+  };
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+  useEffect(() => {
+    setTextAreaElement(textAreaRef.current);
+  }, [textAreaRef]);
+  const setNotificationMode = (
+    e: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+    type: NotificationType,
+  ) => {
+    e.stopPropagation();
+    _setNotificationMode(type);
+  };
   return (
-    <div className={styles["task-component"]}>
+    <div
+      className={styles["task-component"]}
+      onClick={() => {
+        if (notificationMenuActive) setNotificationMenuActive(false);
+      }}
+    >
       <div className="field has-addons has-background-grey-darker columns">
-        <p className="control column is-0 ">
+        <div className="control column is-0 ">
           <button
             className={`has-background-grey-darker ${styles["task-component-button"]}`}
           >
             <Trash
-              className={styles["task-component-icon"]}
+              className={`${styles["task-component-icon"]} has-text-danger`}
               onClick={deleteTask}
             />
           </button>
-        </p>
-        <p className={`control column is-11 ${styles["input-container"]}`}>
-          <input
-            type="text"
+        </div>
+        <div className={`control column is-11 ${styles["input-container"]}`}>
+          <textarea
             value={title}
             onChange={(e) => setTitle(e.target.value)}
             className={`has-background-grey-dark ${styles.input}`}
-            onBlur={() =>
+            ref={textAreaRef}
+            contentEditable
+            onFocus={() => setTextAreaFocused(true)}
+            style={{
+              height: "auto",
+              minHeight:
+                textAreaElement !== null
+                  ? textAreaFocused
+                    ? textAreaElement.scrollHeight
+                    : Math.min(textAreaElement.scrollHeight, 100)
+                  : 0,
+            }}
+            onBlur={() => {
               updateTask({
                 ...task,
                 title: title,
-              })
-            }
+              });
+              setTextAreaFocused(false);
+            }}
           />
-        </p>
-        <p className="control column is-0">
-          <button
-            className={`has-background-grey-darker ${styles["task-component-button"]}`}
+        </div>
+        <div className="control column is-0">
+          <div
+            className={`dropdown ${styles["dropdown-fh"]} ${
+              notificationMenuActive ? "is-active" : ""
+            }`}
+            onClick={() => setNotificationMenuActive(true)}
+            onBlur={() => setNotificationMenuActive(false)}
           >
-            <Bell className={styles["task-component-icon"]} />
-          </button>
-        </p>
-        <p className="control column is-0 ml-5">
+            <div className="dropdown-trigger">
+              <button
+                className={`has-background-grey-darker ${styles["task-component-button"]}`}
+              >
+                {notificationMode === NotificationType.None ? (
+                  <BellOff
+                    className={`${styles["task-component-icon"]} has-text-grey`}
+                  />
+                ) : notificationMode === NotificationType.Non_Intrusive ? (
+                  <Bell
+                    className={`${styles["task-component-icon"]} has-text-grey`}
+                  />
+                ) : (
+                  <Bell
+                    className={`${styles["task-component-icon"]} has-text-info`}
+                  />
+                )}
+              </button>
+            </div>
+            <div className="dropdown-menu">
+              <div className="dropdown-content has-background-grey-darker">
+                <a
+                  href="#"
+                  className="dropdown-item has-text-primary"
+                  onMouseDown={(e) =>
+                    setNotificationMode(e, NotificationType.None)
+                  }
+                >
+                  <BellOff className={"mr-5 has-text-grey"} />
+                  None
+                </a>
+                <a
+                  href="#"
+                  className="dropdown-item has-text-primary"
+                  onMouseDown={(e) =>
+                    setNotificationMode(e, NotificationType.Non_Intrusive)
+                  }
+                >
+                  <Bell className={"mr-5 has-text-grey"} />
+                  Non Intrusive
+                </a>
+                <a
+                  href="#"
+                  className="dropdown-item has-text-primary"
+                  onMouseDown={(e) =>
+                    setNotificationMode(e, NotificationType.Intrusive)
+                  }
+                >
+                  <Bell className={"mr-5 has-text-info"} />
+                  Intrusive
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="control column is-0 ml-5">
           <button
             className={`has-background-grey-darker ${styles["task-component-button"]}`}
           >
             {!task.done ? (
               <Check
-                className={styles["task-component-icon"]}
-                onClick={switchTaskStatus}
+                className={`${styles["task-component-icon"]} has-text-primary`}
+                onMouseDown={switchTaskStatus}
               />
             ) : (
               <X
-                className={styles["task-component-icon"]}
-                onClick={switchTaskStatus}
+                className={`${styles["task-component-icon"]} has-text-danger`}
+                onMouseDown={switchTaskStatus}
               />
             )}
           </button>
-        </p>
+        </div>
       </div>
     </div>
   );
