97c878c8041e08bf6ebb49e48a440bdbe1455fd5
Lucas Ike <lucasmarques@proton.me>
2024-01-23 20:08:16 +0100

    small performance update

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -1,16 +1,25 @@
 "use client";
 
 import moment from "moment/moment";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import styles from "./footer.module.scss";
+import { clearInterval } from "timers";
 let nowProxy = () => moment().format("dd. MMMM Do YYYY, h:mm a");
 const Footer: React.FC = () => {
   const [now, setNow] = useState(() => nowProxy());
+  const [intervalId, setIntervalId] = useState<NodeJS.Timeout | undefined>(
+    undefined,
+  );
+
   useEffect(() => {
-    //TODO make this more efficient somehow
-    const clock = setInterval(() => setNow(nowProxy()), 1000);
-    return () => clearInterval(clock);
-  }, []);
+    const currentDate = moment();
+    const secondsBeforeNextMinute = 60 - currentDate.seconds();
+    console.log(secondsBeforeNextMinute);
+    setTimeout(() => {
+      setNow(nowProxy());
+      const interval = setInterval(() => setNow(nowProxy()), 60000);
+    }, secondsBeforeNextMinute * 1000);
+  }, [intervalId]);
 
   return <h3 className={styles.now}>{now}</h3>;
 };
